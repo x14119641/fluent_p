8f9852c495908216fb3c7848032c44e7f0ecac84
Dani <dgilromero@bloomberg.net>
2020-01-22 16:24:46 +0000

    Added two exmaples from chapter 1

---
+++ ch1/deck_cards.py
@@ -0,0 +1,25 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+suit_values = dict(spades=3, hearts=2, diamons=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamons clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
---
+++ ch1/vector.py
@@ -0,0 +1,27 @@
+from math import hypot
+
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        # return the Euclidean norm: sqrt(x*x + y*y)
+        # This is the lenght of the vector from the origin point (x,y)
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x*scalar, self.y*scalar)
